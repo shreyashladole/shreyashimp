ec508783b46205cecafc90963e8a1dca185c30a5
dell <dell@DESKTOP-IE4IOQG>
2020-02-17 11:34:56 +0530

    this is

--- DemoGit/src/com/cjc/Demo.java
+++
@@ -1,8 +0,0 @@
-package com.cjc;
-
-public class Demo {
-public void testdat(){
-	System.out.println();
-System.out.println("git");
-}
-}
---
+++ Demogit/src/demo.java
@@ -0,0 +1,6 @@
+
+public class demo {
+public void testdata(){
+	System.out.println();
+}
+}
